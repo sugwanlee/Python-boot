aa53235904652dbe2df19df8102737985cd37527
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2024-12-31 21:18:42 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -3,5 +3,4 @@ def solution(str1, str2):
     for i in range(len(str1)):
         answer.append(str1[i])
         answer.append(str2[i])
-    answer = ''.join(answer)
-    return answer
+    return ''.join(answer)
