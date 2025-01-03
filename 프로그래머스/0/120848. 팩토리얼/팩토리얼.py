ce28ba1406e35148e7b61c6abccc5f7a8df69bf8
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2025-01-03 09:32:55 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,8 +1,8 @@
 def solution(n):
-    fac = 1
-    for i in range(1,n+1):
-        fac = fac*i
-        if fac == n:
-            return i
-        elif fac >n:
-            return i-1
+    fac = 1                     # 팩토리얼 값을 담을 변수 선언
+    for i in range(1,n+1):      # 1~n까지 돌리는 for문 생성
+        fac = fac*i             # 팩토리얼(i!) 
+        if fac == n:            # i! 값이 n과 같으면
+            return i            # i를 반환
+        elif fac >n:            # i! 값이 n보다 크면
+            return i-1          # 바로 이전 i값(i-1)을 반환
