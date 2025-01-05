3d4ac78f678be65c407af4109e4daedafa0bb787
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2025-01-06 07:00:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,11 +1,11 @@
 def solution(my_string):
-    answer = 0
-    number = "0"
-    for i in my_string:
-        if i in "0123456789":
-            number += i
+    answer = 0                      # 답으로 제출할 변수 선언
+    number = "0"                    # 연속된 수는 하나의 수로 간주하기 위한 number 변수
+    for i in my_string:             # my_string에서 하나씩 꺼내오기
+        if i.isdigit():       # i가 자연수인지 판별
+            number += i             # number에 숫자인 문자 더해주기
         else:
-            answer += int(number)
-            number = "0"
-    answer += int(number)
+            answer += int(number)   # 숫자 외의 값이 나오면 number에 들어있는 값 숫자로 바꿔서 answer에 더해주기
+            number = "0"            # 더한 뒤 number 초기화
+    answer += int(number)           # my_string의 마지막 index위치의 값이 숫자일 경우를 위한 코드
     return answer
