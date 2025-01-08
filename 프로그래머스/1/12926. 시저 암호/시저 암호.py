0caf21516fac6300aaa50b4f65cf9b6856c0140e
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2025-01-08 10:23:55 +0900

    [level 1] Title: 시저 암호, Time: 8.48 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,8 +1,8 @@
 def solution(s, n):
     aski = list(s)
     aski = list(map(lambda x : ord(x), aski))
-    b = list(range(65,91)) + list(range(65,91)) + list(range(65,91))*(n//25)
-    c = list(range(97,123)) + list(range(97,123)) + list(range(65,91))*(n//25)
+    b = list(range(65,91)) + list(range(65,91))
+    c = list(range(97,123)) + list(range(97,123))
     answer = []
     for i in aski:
         if i in b:
@@ -12,6 +12,4 @@ def solution(s, n):
         else:
             answer.append(32)
     answer = ''.join(map(chr,answer))
-    # aski_num = list(map(lambda x : ord(x)+n, aski))
-    
     return answer
