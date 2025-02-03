0c462a9d177d90d9a1d8c211736ae026016adbc5
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2025-02-04 01:26:23 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,12 +1,12 @@
 def solution(keyinput, board):
     answer = [0, 0]
     for arrow in keyinput:
-            if arrow == "up" and answer[1] != board[1]//2:
-                answer[1] += 1
-            if arrow == "down" and answer[1] != -abs(board[1]//2):
-                answer[1] -= 1
-            if arrow == "left" and answer[0] != -abs(board[0]//2):
-                answer[0] -= 1
-            if arrow == "right" and answer[0] != board[0]//2:
-                answer[0] += 1
+        if arrow == "up" and answer[1] != board[1]//2:
+            answer[1] += 1
+        if arrow == "down" and answer[1] != -(board[1]//2):
+            answer[1] -= 1
+        if arrow == "left" and answer[0] != -(board[0]//2):
+            answer[0] -= 1
+        if arrow == "right" and answer[0] != board[0]//2:
+            answer[0] += 1
     return answer
