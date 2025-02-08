8a718534d2d028bc4091d94e640e37062fd234d4
Orange_00 <147833780+sugwanlee@users.noreply.github.com>
2025-02-09 03:15:28 +0900

    [level 0] Title: 배열 만들기 6, Time: 236.24 ms, Memory: 18.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if stk == []:  
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] == arr[i]:
+            del stk[-1]
+            i += 1
+        elif stk[-1] != arr[i]:
+            stk.append(arr[i])
+            i += 1
+    if stk == []:
+        return [-1]
+    return stk
